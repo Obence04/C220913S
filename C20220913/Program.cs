692e806bf972f414ab16b6d5079f9de2969a9817
Bence Oláh <olah.bence@verebelyszki.hu>
2022-09-16 19:25:19 +0200

    Added File read

--- C20220913/Program.cs
+++ C20220913/Program.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace C20220913
 {
     internal class Program
     {
+        private static void Read()
+        {
+            FileStream fs = new FileStream(@"..\..\kuldetesek.csv", FileMode.Open);
+            StreamReader sr = new StreamReader(fs, Encoding.UTF8);
+            while(!sr.EndOfStream)
+            {
+                List_Shuttle.Add(new Shuttle(sr.ReadLine().Split(';')));
+            }
+            sr.Close(); fs.Close();
+        }
+
+        static List<Shuttle> List_Shuttle = new List<Shuttle>();
+
         static void Main(string[] args)
         {
-
+            Read();
             Console.ReadKey(true);
         }
     }
---
+++ C20220913/Shuttle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Shuttle
+{
+	public string MsCode;
+	public DateTime Launch;
+	public string Name;
+	public int HoursAway, MinutesAway;
+	public string Base;
+	public int Crew;
+	public Shuttle(string[] line)
+	{
+		this.MsCode = line[0];
+		this.Launch = Convert.ToDateTime(line[1]);
+		this.Name = line[2];
+		this.HoursAway = Convert.ToInt32(line[3]);
+		this.MinutesAway = Convert.ToInt32(line[4]);
+		this.Base = line[5];
+		this.Crew = Convert.ToInt32(line[6]);
+	}
+}
